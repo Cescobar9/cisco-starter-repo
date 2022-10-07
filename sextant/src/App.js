868dfe51a5b9aad81637d4a8f6b51b2b369f2c64
Cescobar9 <chr.escobar1998@gmail.com>
2022-10-07 16:43:19 -0700

    2nd Task

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Banner title="Chris's Sextant"/>
+        <div className='col'>
+          <Exhibit title = "Hai ^.^/" description="I love Cookies" />
+          <Exhibit title = "Hai :<" description="I hate Cookies" />
+          <Exhibit title = "Hai!" description="I love Cookies" />
+        </div>
+      </div>
+    );
+  }
 }
+
+class Banner extends React.Component{
+  render(){
+    return (
+      <div className='Banner'>
+        <div>
+          <h1 className='Banner-text'> {this.props.title} </h1>
+        </div>
+      </div>
+    ) ;
+  }
+}
+
+class Exhibit extends React.Component{
+  render(){
+    return(
+      <div className='Card'>
+        <h2 className='Card-Data-Header'>{this.props.title}</h2>
+        <p className='Card-Text'>{this.props.description}</p>
+      </div>
+    );
+  }
+}
+
 
 export default App;
